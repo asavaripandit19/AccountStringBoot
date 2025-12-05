9afc018913984979f3d6dc0eefeb6d5af7e23887
asavaripandit19 <asavaripandit2020@gmail.com>
2025-12-05 16:47:09 +0530

    jlygfyulgg

--- Account-1/src/main/java/com/account/demo/Account1Application.java
+++ Account-1/src/main/java/com/account/demo/Account1Application.java
@@ -9,6 +9,14 @@ public class Account1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Account1Application.class, args);
 		System.out.println("hellow programmer");
+		System.out.println("hellow programmer");
+		System.out.println("hellow programmer");
+		System.out.println("hellow programmer");
+		System.out.println("hellow programmer");
+		System.out.println("hellow programmer");
+		System.out.println("hellow programmer");
+		System.out.println("hellow programmer");
+		
 		
 	}
 
